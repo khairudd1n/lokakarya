60436a10c53270e5509be77c50688cd03092762d
emikhlas <m.ikhlas2001@gmail.com>
2024-11-21 13:26:20 +0700

    add userlist and auth service

---
+++ src/app/core/models/division.model.ts
@@ -0,0 +1,8 @@
+export interface Division {
+    id: string;
+    division_name: string;
+    created_at: string;
+    created_by: string | null;
+    updated_at: string | null;
+    updated_by: string | null;
+}
