596f671f26b7bcec5a806239a30c26d974a9dba3
emikhlas <m.ikhlas2001@gmail.com>
2024-11-21 11:07:37 +0700

    Revert "auth service, usergetall service done"
    This reverts commit 85084479ee0e1ad68d1cff686624c460c2c61f14.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GroupAchievementComponent } from './group-achievement/group-achievement.component';
-import { UserListComponent } from './features/user/user-list/user-list.component';
-import { LoginComponent } from './features/auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GroupAchievementComponent, UserListComponent, LoginComponent],
+  imports: [RouterOutlet, GroupAchievementComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
